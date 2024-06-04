a1749c271710aeb2f0d5ac03cd94a8291b467b00
unknown <zenagsuma5@gmail.com>
2024-06-04 09:34:36 -0300

    Modificacion del main

--- MedFiles o AssistMedic/assets/js/reg-js.js
+++
@@ -1,9 +0,0 @@
-const elForm = document.getElementById("register")
-
-elForm.addEventListener("submit" , (e) => {
-    e.preventDefault()
-
-    const formData = new formData(e.target)
-    formData.get("password")
-    formData.get("confirm-password")
-})
